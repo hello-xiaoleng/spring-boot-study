a2960f912980fe8012be04a199087a97620b1374
chenjia <chenjia02@@chinatopcredit.com>
2018-11-20 13:41:23 +0800

    test dubbo 第二版

--- member/src/main/java/com/xiaoleng/member/MemberApplication.java
+++ member/src/main/java/com/xiaoleng/member/MemberApplication.java
@@ -1,8 +1,10 @@
 package com.xiaoleng.member;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo
 @SpringBootApplication
 public class MemberApplication {
 
